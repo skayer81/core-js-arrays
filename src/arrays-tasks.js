c3008f7877d3c7c92b6aadae0276bc1cdfe40cec
Sergey <seleznev-krista@mail.ru>
2023-12-26 20:49:45 +0300

    feat: add findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -367,8 +367,8 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, elem) => acc + (item === elem) * 1, 0);
 }
 
 /**
