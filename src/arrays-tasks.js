0051fbbf0e83b8e71381d719fda64a0f2da933f7
Sergey <seleznev-krista@mail.ru>
2023-12-26 20:43:34 +0300

    feat: add getPositivesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -297,8 +297,11 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.reduce(
+    (acc, elem) => acc + +(typeof elem === 'number' && elem > 0),
+    0
+  );
 }
 
 /**
