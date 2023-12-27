8ece405415e8b4ecb5d32eeb474e23e03fe294cb
Sergey <seleznev-krista@mail.ru>
2023-12-27 11:58:15 +0300

    feat: add getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -434,8 +434,12 @@ function sortCitiesArray(/* arr */) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(Array(n).fill(0))
+    .map((elem, index) =>
+      elem.map((elem2, index2) => (index2 === index ? 1 : 0))
+    );
 }
 
 /**
