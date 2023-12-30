980552ee73690cab528119cb9afe726f427e833c
Sergey <seleznev-krista@mail.ru>
2023-12-31 01:33:44 +0300

    feat: add findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -487,8 +487,14 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  return (
+    nums
+      .map((elem, index) => (index === 0 || elem < nums[index - 1] ? 1 : 2))
+      .join('')
+      .split('1')
+      .sort((a, b) => b.length - a.length)[0].length + 1
+  );
 }
 
 /**
