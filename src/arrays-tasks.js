409664fdafc0ecbbdecb0241745bf21994400bd5
Sergey <seleznev-krista@mail.ru>
2023-12-31 04:09:03 +0300

    feat: add shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -544,8 +544,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? [...arr.slice(arr.length - n), ...arr.slice(0, arr.length - n)]
+    : [...arr.slice(-n), ...arr.slice(0, -n)];
 }
 
 /**
